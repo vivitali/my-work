a50aea57922d6d3c94ddd49ece341c402e069edb
Vitalii <vasinkevych@gmail.com>
2018-01-03 19:03:33 +0200

    added alterations route, json, change webpack config for moment js

---
+++ src/compoents/Alter/Alter.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Icon } from '../../compoents/Icon/Icon';
+import './alter.scss';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+export const Alter = ({ alterItem }) => {
+  moment.locale('uk');
+  const parseTime = time => moment(time, 'HH:mm:ss');
+  const formattedDiff = (end, start) =>
+    moment.duration(parseTime(end).diff(parseTime(start))).humanize();
+
+  return (
+    <div className="alter-card">
+      <Link to={`alters/${alterItem.id}`}>
+        <div className="main-block">
+          <div>
+            <span className="title">Початок зміни</span>{' '}
+            <Icon icon="clock-o" />
+            <span className="value">{parseTime(alterItem.startTime).format('HH:mm')}</span>
+          </div>
+          <div>
+            <span className="title">Кінець зміни</span>{' '}
+            <Icon className="my-icon" icon="clock-o" />
+            <span className="value">{parseTime(alterItem.endTime).format('HH:mm')}</span>
+          </div>
+          <div>
+            <span className="title">Тривалість</span>{' '}
+            <span className="value">{formattedDiff(alterItem.endTime, alterItem.startTime)}</span>
+          </div>
+        </div>
+      </Link>
+      <span className="button">
+        <Icon icon="pencil" />
+      </span>
+      <Link to={`alter/${alterItem.id}`}>
+        <div className="header">
+          <div className="name">{alterItem.name}</div>
+        </div>
+      </Link>
+    </div>
+  );
+};
--- src/containers/Alterations/Alterations.js
+++ src/containers/Alterations/Alterations.js
@@ -1,9 +1,15 @@
 import React, { PureComponent } from 'react';
-// import { AlterationCard } from '';
-// import './alterations.scss';
-
+import alters from '../../json/alter.json';
+import { Alter } from '../../compoents/Alter/Alter';
 export default class Alterations extends PureComponent {
   render() {
-    return <div className="container main-block">Типи змін</div>;
+    return (
+      <div className="container main-block">
+        {alters &&
+          alters.map((item, i) => (
+            <Alter alterItem={item} key={`${item.id}_${i}`} />
+          ))}
+      </div>
+    );
   }
 }
